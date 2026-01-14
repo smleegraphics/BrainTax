9e19b98cbdeb85e8ae37bcd8360d7abeef17fc3b
smleegraphics <smlee.graphics@gmail.com>
2026-01-13 17:46:20 -0800

    Initial Commit

---
+++ BrainTax/BrainTaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainTaxApp.swift
+//  BrainTax
+//
+//  Created by Samantha Lee on 1/13/26.
+//
+
+import SwiftUI
+
+@main
+struct BrainTaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
